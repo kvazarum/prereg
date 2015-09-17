415e00b14f1d1e8d36fe31406e5947a6a96cef4f
kvazarum <igor.kvazar@google.com>
2015-09-17 12:53:13 +0300

    first commit

---
+++ frontend/views/requests/specialist-report.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\models\Requests */
+/* @var $form ActiveForm */
+?>
+<div class="specialist-report">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+
+    
+        <div class="form-group">
+
+        </div>
+    <?php ActiveForm::end(); ?>
+
+</div><!-- specialist-report -->
