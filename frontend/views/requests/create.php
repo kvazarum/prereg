8f80fbe9df14afe05f7fdf917987efd3f4f0e8f6
kvazarum <igor.kvazar@gmail.com>
2015-09-22 22:25:13 +0300

    изменён формат вывода даты

--- frontend/views/requests/create.php
+++ frontend/views/requests/create.php
@@ -15,7 +15,21 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="requests-create">
 
     <h1><?= Html::encode($this->title).' '.$model->specialist->doctor->name ?></h1>
-    <?php echo '<h3>Время приёма: '.$model->start_time.'</h3>'; ?>
+    <?php
+        $str_time = $model->start_time;
+        $str_time = explode(' ', $str_time);
+        $date = $str_time[0];
+        $str_time = explode(':', $str_time[1]);
+        $hours = $str_time[0];
+        $minutes = $str_time[1];
+        $date = explode('-', $date);
+        $day = $date[2];
+        $month = $date[1];
+        $year = $date[0];
+        $str_time = $day.'-'.$month.'-'.$year.'г. '.$hours.'ч.'.' '.$minutes.' мин.';
+        
+        echo '<h3>Время приёма: '.$str_time.'</h3>'; 
+    ?>
 
     <?= $this->render('_form', [
         'model' => $model,
