6757c40002e3770fb914c27386b71c365c5c759e
kvazarum <igor.kvazar@gmail.com>
2015-09-23 20:35:49 +0300

    Перевод строки при удалении

--- frontend/views/user/view.php
+++ frontend/views/user/view.php
@@ -19,7 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Удалить', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => 'Вы уверены что хотите удалить этого пользователя?',
                 'method' => 'post',
             ],
         ]) ?>
