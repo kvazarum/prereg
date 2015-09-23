f0db941454024eaaeb9181758577ce4371ec23f1
kvazarum <igor.kvazar@google.com>
2015-09-23 09:17:48 +0300

    добавлен пункт меню "Добавить пользователя"

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -61,6 +61,7 @@ AppAsset::register($this);
             "items" => [
                 ['label' => 'Генерация графиков', 'url' => ['/records/admin-occupations']],
                 ['label' => 'Графики врачей', 'url' => ['/records/index']],
+                ['label' => 'Регистрация', 'url' => ['/site/signup']]
             ]];        
 //        $menuItems[] = ['label' => 'Регистрация', 'url' => ['/site/signup']];
         $menuItems[] = [
