327919b82e4216da915b39d65b8437bdde694cc2
kvazarum <igor.kvazar@google.com>
2015-09-24 08:20:05 +0300

    Добавлена кнопка "Изменить пароль"

--- frontend/views/user/view.php
+++ frontend/views/user/view.php
@@ -23,6 +23,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 'method' => 'post',
             ],
         ]) ?>
+        <?= Html::a('Изменить пароль', ['change-password', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
     </p>
 
     <?= DetailView::widget([
