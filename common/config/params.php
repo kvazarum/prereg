51475647093730a103362dcb4d803be665a2a93b
kvazarum <igor.kvazar@google.com>
2015-09-25 09:20:54 +0300

    Добавлено указание на русский язык приложения

--- common/config/params.php
+++ common/config/params.php
@@ -3,4 +3,6 @@ return [
     'adminEmail' => 'igor.kvazar@gmail.com',
     'supportEmail' => 'igor.kvazar@gmail.com',
     'user.passwordResetTokenExpire' => 3600,
+    'language' => 'ru-RU',
+    'sourceLanguage' => 'ru-RU',
 ];
