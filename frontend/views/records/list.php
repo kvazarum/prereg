cdab6f096da18a42493e1befba672a65f80182ec
kvazarum <igor.kvazar@google.com>
2015-09-25 10:56:09 +0300

    Изменён дизайн

--- frontend/views/records/list.php
+++ frontend/views/records/list.php
@@ -22,14 +22,14 @@ $this->title = 'Выбор времени приёма';
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Выбор даты'), 'url' => ['specialists/viewss', 'id' => $spec->occupation_id]];
 $this->params['breadcrumbs'][] = $this->title;
 
-    echo Html::beginTag('div', ['class' => "jumbotron"]);
+    echo Html::beginTag('div');
         echo Html::beginTag('div', ['class' => "page-header"]);
             echo Html::tag('h3', Html::encode($this->title));
             echo Html::tag('h2', $doctor->name);
             echo Html::tag('h4',$doctor->description);
 
             $month = Records::$monthsFull[(int)date('m', $date)];
-            echo '<p><strong>'.date("d", $date).' '.$month.' '.date('Y', $date).'</strong></p>';
+            echo '<h4><strong>'.date("d", $date).' '.$month.' '.date('Y', $date).'</strong></h4>';
         echo Html::endTag('div');
 
 ?>
@@ -37,10 +37,17 @@ $this->params['breadcrumbs'][] = $this->title;
     
     <?php 
         $records = Records::getRecords($date, $id);
-
-        echo '<div class="jumbotron">';
+//        $count = 0;
+        $hour = 0;
+        echo Html::beginTag('div', ['class' => '']);
+        echo Html::beginTag('p');
         for ($i = 0; $i <count($records); $i++)
         {
+            $data = strtotime($records[$i]->start_time);
+            if ($i == 0)
+            {
+                $hour = date("H", $data);
+            }
             if (Yii::$app->user->isGuest)
             {
                 $url = Url::to(['requests/create', 'id' => $records[$i]->id]);
@@ -49,13 +56,21 @@ $this->params['breadcrumbs'][] = $this->title;
             {
                 $url = Url::to(['records/update', 'id' => $records[$i]->id]);
             }
-            $text = strtotime($records[$i]->start_time);
-            $text = date("H:i", $text);
-            $classes = 'btn btn-lg btn-info';
-            echo Html::beginTag('p');
-                echo Html::a($text, $url, ['class' => $classes]);
-            echo Html::endTag('p');
-        }    
+            
+            $text = date("H:i", $data);
+            $classes = 'btn btn-lg btn-info list-group-item modal-content';
+            
+            if ($hour != date("H", $data))
+            {
+//                $count = 0;
+                $hour = date("H", $data);
+                echo Html::endTag('p');
+                echo Html::beginTag('p');
+            }
+//            $count++;
+            echo Html::a($text, $url, ['class' => $classes]);
+        }
+        echo Html::endTag('p');
         echo Html::endTag('div');
     ?>
 </div>
