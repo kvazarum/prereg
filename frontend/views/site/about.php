b37f6549b60f9b0b1eb22c04a2ca61240b9fa238
kvazarum <igor.kvazar@google.com>
2015-09-30 12:08:13 +0300

    изменены данные

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -8,11 +8,28 @@ $this->title = 'О нас';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+    
+    <?php
+        echo html::beginTag('div', ['class' => 'col-lg-4']);
+            echo html::beginTag('div', ['class' => 'panel panel-info ']);
+            echo Html::beginTag('div', ['class' => 'panel-heading'] );
+            echo Html::tag('h1', Html::encode($this->title), ['class' => 'panel-title'] );
+            echo Html::endTag('div');
 
-    <p>Больница находится по адресу: Тверская область, г.Тверь, Петербургское шоссе, 105</p>
-    <p>Телефон: 77-53-77, 77-54-77, 47-51-58</p>
-    <p>Телефон для записи на прием, исследования : 47-51-58, 77-54-65</p>
+            echo Html::beginTag('div', ['class' => 'panel-body'] );
+    ?>
+    <p><b>Больница находится по адресу:</b><br />
+        Тверская область, г.Тверь, Петербургское шоссе, 105</p>
+    <p><b>Телефон для записи на прием за наличный расчёт:</b><br>
+        47-51-58, 77-54-65
+    </p>
 
+    <p><b>Телефон для записи на прием по полисам ДМС:</b><br />
+        77-53-77, 77-54-77</p>
+    <?php
+                echo Html::endTag('div');
+            echo Html::endTag('div');
+        echo Html::endTag('div');
+            ?>
     
 </div>
