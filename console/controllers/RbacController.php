33919187e6a17824c8c7e1d191265f3aa77eb262
kvazarum <igor.kvazar@google.com>
2015-10-05 13:14:52 +0300

    added, first commit file for creating roles, riles

---
+++ console/controllers/RbacController.php
@@ -0,0 +1,39 @@
+<?php
+namespace console\controllers;
+use Yii;
+use yii\console\Controller;
+use common\components\rbac\UserRoleRule;
+use yii\rbac\DbManager;
+class RbacController extends Controller
+{
+    public function actionInit()
+    {
+//        $auth = Yii::$app->authManager;
+        $auth = new DbManager;
+        $auth->removeAll(); //удаляем старые данные
+        //Создадим права для доступа к админке
+        $dashboard = $auth->createPermission('dashboard');
+        $dashboard->description = 'Админ панель';
+        $auth->add($dashboard);
+        //Включаем наш обработчик
+        $rule = new UserRoleRule();
+        $auth->add($rule);
+        //Добавляем роли
+        $user = $auth->createRole('user');
+        $user->description = 'Пользователь';
+        $user->ruleName = $rule->name;
+        $auth->add($user);
+        $moder = $auth->createRole('moder');
+        $moder->description = 'Модератор';
+        $moder->ruleName = $rule->name;
+        $auth->add($moder);
+        //Добавляем потомков
+        $auth->addChild($moder, $user);
+        $auth->addChild($moder, $dashboard);
+        $admin = $auth->createRole('admin');
+        $admin->description = 'Администратор';
+        $admin->ruleName = $rule->name;
+        $auth->add($admin);
+        $auth->addChild($admin, $moder);
+    }
+}
