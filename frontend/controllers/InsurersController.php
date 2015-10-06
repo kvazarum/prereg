48773be4c25bb3ab76c85db5e2cf5587e8762e6e
kvazarum <igor.kvazar@google.com>
2015-10-06 08:28:49 +0300

    added behaviors

--- frontend/controllers/InsurersController.php
+++ frontend/controllers/InsurersController.php
@@ -17,6 +17,22 @@ class InsurersController extends Controller
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['create', 'update', 'delete',
+                    'view', 'index',
+                ],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['moder']
+                    ],
+                    [
+                        'allow' => false,
+                        'roles' => ['?']
+                    ]                    
+                ]
+            ],             
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
