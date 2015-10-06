1e83ddac8d944b4a2f81dcde71effc0334b12b7a
kvazarum <igor.kvazar@google.com>
2015-10-06 11:50:36 +0300

    added 'urlManager'

--- common/config/main.php
+++ common/config/main.php
@@ -9,5 +9,12 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                'module/<module:\w+>/<controller:\w+>/<action:\w+>' => '<module>/<controller>/<action>',
+            ]
+        ],        
     ],
 ];
