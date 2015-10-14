12fb53604c76a22bc638b2dc383be89d2b95214c
kvazarum <igor.kvazar@google.com>
2015-10-14 09:56:20 +0300

    added timezone

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -9,6 +9,7 @@ $params = array_merge(
 return [
     'id' => 'app-frontend',
     'language' => 'ru-RU',
+    'timeZone' => 'Europe/Moscow',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'modules' => [
