867b46e054f1632e5ea7f7c30a8dd04b1675681e
kvazarum <igor.kvazar@google.com>
2015-10-14 11:18:48 +0300

    добавлено поведение, запрет использования модуля всем кроме admin

--- frontend/modules/admin/Module.php
+++ frontend/modules/admin/Module.php
@@ -1,10 +1,26 @@
 <?php
 
 namespace frontend\modules\admin;
+use yii\filters\AccessControl;
 
 class Module extends \yii\base\Module
 {
     public $controllerNamespace = 'frontend\modules\admin\controllers';
+    
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['admin'],
+                    ],
+                ],
+            ],
+        ];
+    }    
 
     public function init()
     {
